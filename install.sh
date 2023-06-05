446afe1d4a8aae4b53e6a27036c9a5bb966a0f81
arm321689 <57206825+arm321689@users.noreply.github.com>
2023-06-05 09:02:33 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+#!/data/data/com.termux/files/usr/bin/bash
 echo '"Installing....'
 tar -zxf /sdcard/termux-backup.tar.gz -C /data/data/com.termux/files --recursive-unlink --preserve-permissions
 echo '"Finish....'
